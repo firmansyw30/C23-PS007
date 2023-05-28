8f6f624061a43262db3e737fb1b8b1aaaa90753b
bayu afriyadi <bayuafriyadi5@gmail.com>
2023-05-28 22:48:57 +0700

    update time format and shimmer

--- app/src/main/java/com/capstone/trashtotreasure/view/ui/adapter/ArticleAdapter.kt
+++ app/src/main/java/com/capstone/trashtotreasure/view/ui/adapter/ArticleAdapter.kt
@@ -7,6 +7,9 @@ import com.bumptech.glide.Glide
 import com.capstone.trashtotreasure.databinding.ItemArticleBinding
 import com.capstone.trashtotreasure.model.data.remote.response.ArticlesItem
 import com.capstone.trashtotreasure.view.ui.home.HomeFragment
+import org.ocpsoft.prettytime.PrettyTime
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ArticleAdapter(private val context: HomeFragment, private var list: List<ArticlesItem?>?) :  RecyclerView.Adapter<ArticleAdapter.MyViewHolder>() {
 
@@ -20,8 +23,14 @@ class ArticleAdapter(private val context: HomeFragment, private var list: List<A
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val news = list?.get(position)
 
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+        dateFormat.timeZone = TimeZone.getTimeZone("ID")
+        val time = dateFormat.parse(news?.publishedAt!!)?.time
+        val prettyTime = PrettyTime(Locale.getDefault())
+        val date = prettyTime.format(time?.let { Date(it) })
+
         holder.binding.tvItemTitle.text = news?.title
-        holder.binding.tvItemPublishedDate.text = news?.publishedAt
+        holder.binding.tvItemPublishedDate.text = date
 
 
         Glide.with(holder.itemView.context)
